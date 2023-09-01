e9ef0dca28d1ce026188e2ee0a9d436619d78968
Mubashir Reshamwala <mubresham@gmail.com>
2023-09-01 18:37:40 +0530

    fix: token

--- release.config.js
+++ release.config.js
@@ -4,7 +4,8 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/npm',
+    ['@semantic-release/npm', { npmPublish: true }],
+    ['@semantic-release/git', { message: 'chore(release): ${nextRelease.version} [skip ci]' }],
     '@semantic-release/github',
   ],
 };
