e171334354653f1bf93d073a60dfcf6b7473e8a2
Mubashir Reshamwala <mubresham@gmail.com>
2023-09-25 09:42:01 +0530

    feat: something new

--- release.config.js
+++ release.config.js
@@ -1,11 +1,21 @@
+const branch = process.env.CI_COMMIT_BRANCH;
+
 module.exports = {
-  branches: [{name: 'alpha', prerelease: true}, 'master'],
+  branches: [{ name: "alpha", prerelease: true }, "master"],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    ['@semantic-release/npm', { npmPublish: true }],
-    ['@semantic-release/git', { message: 'chore(release): ${nextRelease.version} [skip ci]' }],
-    ['@semantic-release/github', { branch: 'master' }],
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+
+    ...(config.branches.some(
+      (it) => it === branch || (it.name === branch && !it.prerelease)
+    ) && [
+      "@semantic-release/changelog",
+      [
+        "@semantic-release/git",
+        { message: "chore(release): ${nextRelease.version} [skip ci]" },
+      ],
+    ]),
+    ["@semantic-release/npm", { npmPublish: true }],
+    ["@semantic-release/github", { branch: "master" }],
   ],
 };
