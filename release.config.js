b513986d22a615d4b3ca33c51154498e1beb4919
Mubashir Reshamwala <mubresham@gmail.com>
2023-09-29 17:46:59 +0530

    chore: skip git

--- release.config.js
+++ release.config.js
@@ -1,17 +1,28 @@
-console.log('branch', process.env.GITHUB_REF_NAME)
+console.log("branch", process.env.GITHUB_REF_NAME);
 
 const branch = process.env.GITHUB_REF_NAME;
-const prerelease = branch !== 'master';
-const prereleaseChangelog = prerelease && 'CHANGELOG.alpha.md';
+const prerelease = branch !== "master";
+const prereleaseChangelog = prerelease && "CHANGELOG.alpha.md";
 
-module.exports = {
-  branches: [{name: '*eg-*', prerelease: true}, 'master'],
+const config = {
+  branches: [{ name: "*eg-*", prerelease: true }, "master"],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    ['@semantic-release/changelog'],
-    ['@semantic-release/npm', { npmPublish: true }],
-    ['@semantic-release/git', { message: 'chore(release): ${nextRelease.version} [skip ci]', branch: 'master' }],
-    ['@semantic-release/github', { branch: 'master' }]
-  ].filter(p => p),
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/changelog"],
+    ["@semantic-release/npm", { npmPublish: true }],
+  ],
 };
+if (!prerelease) {
+  config.plugins.push(
+    [
+      "@semantic-release/git",
+      {
+        message: "chore(release): ${nextRelease.version} [skip ci]",
+        branch: "master",
+      },
+    ],
+    ["@semantic-release/github", { branch: "master" }]
+  );
+}
+module.exports = config;
