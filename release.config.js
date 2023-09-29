afef704ea1a7ff787475e3c78a87c6b80c1ff795
Mubashir Reshamwala <mubresham@gmail.com>
2023-09-29 17:25:32 +0530

    feat: new feature

--- release.config.js
+++ release.config.js
@@ -4,16 +4,14 @@ const branch = process.env.GITHUB_REF_NAME;
 const prerelease = branch !== 'master';
 const prereleaseChangelog = prerelease && 'CHANGELOG.alpha.md';
 
-const config = {
+module.exports = {
   branches: [{name: '*eg-*', prerelease: true}, 'master'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ['@semantic-release/changelog', { changelogFile: prereleaseChangelog}],
+    ['@semantic-release/changelog'],
     ['@semantic-release/npm', { npmPublish: true }],
-    ['@semantic-release/git', { message: 'chore(release): ${nextRelease.version} [skip ci]', assets: prereleaseChangelog && [prereleaseChangelog] }],
+    ['@semantic-release/git', { message: 'chore(release): ${nextRelease.version} [skip ci]', branch: 'master' }],
     ['@semantic-release/github', { branch: 'master' }]
-  ],
+  ].filter(p => p),
 };
-
-module.exports = config;
