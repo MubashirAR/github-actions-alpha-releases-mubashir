6b466bd8a6ca8ced5208d10b5e6f7e3345bd26d5
Mubashir Reshamwala <mubresham@gmail.com>
2023-09-29 17:17:15 +0530

    fix: trye

--- release.config.js
+++ release.config.js
@@ -5,18 +5,15 @@ const prerelease = branch !== 'master';
 const prereleaseChangelog = prerelease && 'CHANGELOG.alpha.md';
 
 const config = {
-  branches: [{name: '[a-z0-9_-]*', prerelease: true}, 'master'],
+  branches: [{name: '*eg-*', prerelease: true}, 'master'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     ['@semantic-release/changelog', { changelogFile: prereleaseChangelog}],
     ['@semantic-release/npm', { npmPublish: true }],
     ['@semantic-release/git', { message: 'chore(release): ${nextRelease.version} [skip ci]', assets: prereleaseChangelog && [prereleaseChangelog] }],
+    ['@semantic-release/github', { branch: 'master' }]
   ],
 };
-
-if(!prerelease) {
-  config.plugins.push(['@semantic-release/github', { branch: 'master' }])
-}
 
 module.exports = config;
